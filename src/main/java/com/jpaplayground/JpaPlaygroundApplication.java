ab528497ea7ceb5a0cb2a750181b0bc7c0a02e5f
honeySleepr <gruzzimo@naver.com>
2022-08-27 14:00:19 +0900

    🎉 jpaPlayground 간이 프로젝트 시작

---
+++ src/main/java/com/jpaplayground/JpaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jpaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpaplayground/JpaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
