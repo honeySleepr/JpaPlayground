16f2b01bbd2d1eab9a8fc334fdf2509ba24bdf6b
honeySleepr <gruzzimo@naver.com>
2022-09-20 15:40:25 +0900

    ✨ 모든 예외에 대해 일관된 응답을 하도록 로직 추가
    + GlobalExceptionHandler에 일부 입력값 관련 에러를 처리하는 handler
    추가
    + GlobalExceptionHandler에 모든 예외를 처리하는 handler 추가
    + FieldError 표시를 위해 ErrorResponse에 내부 클래스 ErrorDetail
    추가

--- src/main/java/com/jpaplayground/global/exception/ErrorCode.java
+++ src/main/java/com/jpaplayground/global/exception/ErrorCode.java
@@ -8,7 +8,9 @@ public enum ErrorCode {
 
 	/* TODO: 도메인 별로 Exception을 만들지 않고 BusinessException 하나로 퉁치고 ErrorCode만으로 구분하는 방법의 단점은? */
 
-	PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "Product not found");
+	PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "Product not found"),
+	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid input value"),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
 	private final HttpStatus status;
 	private final String message;
--- src/main/java/com/jpaplayground/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jpaplayground/global/exception/GlobalExceptionHandler.java
@@ -3,17 +3,53 @@ package com.jpaplayground.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+	/**
+	 * `@Valid` 검증 실패 시 발생하는 에러를 처리한다
+	 */
+	@ExceptionHandler(BindException.class)
+	public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+		log.error("handleBindException", e);
+		ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+		return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatus()));
+	}
+
+	/**
+	 * 메서드 인자로 입력된 값의 타입이 맞지 않을 경우 발생하는 에러를 처리한다 (ex:@RequestParam Long id에 문자값 읿력)
+	 */
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+		MethodArgumentTypeMismatchException e) {
+		log.error("handleTypeMismatchException", e);
+		ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_INPUT_VALUE);
+		return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatus()));
+	}
+
+	/**
+	 * 비즈니스 로직 관련 에러를 처리한다
+	 */
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
 		log.error("handleBusinessException", e);
 		ErrorResponse response = new ErrorResponse(e.getErrorCode());
+		return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatus()));
+	}
+
+	/**
+	 * 그 외에 따로 처리해주지 않은 에러는 여기에서 처리해줌으로써 모든 error에 대해 일관된 형태의 API Response로 응답한다
+	 */
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleAllException(Exception e) {
+		log.error("handleAllException", e);
+		ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
 		return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatus()));
 	}
 }
