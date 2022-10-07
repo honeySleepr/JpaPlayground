e22e593fba23f7b097eb740330c5346f869d9e8f
honeySleepr <gruzzimo@naver.com>
2022-10-07 13:17:09 +0900

    ✨ OAuth 로그인 시 CSRF 방지를 위한 state param 추가

--- src/main/java/com/jpaplayground/global/login/LoginFilter.java
+++ src/main/java/com/jpaplayground/global/login/LoginFilter.java
@@ -1,6 +1,7 @@
 package com.jpaplayground.global.login;
 
 import java.io.IOException;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.Filter;
@@ -12,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +22,9 @@ import org.springframework.stereotype.Component;
 public class LoginFilter implements Filter {
 
 	private static final Pattern pattern = Pattern.compile("/login/(.*)$");
+	public static final String CLIENT_ID = "client_id";
+	public static final String STATE = "state";
+	public static final String REDIRECT_URI = "redirect_uri";
 	private final OAuthPropertyHandler oAuthPropertyHandler;
 
 	@Override
@@ -31,7 +37,16 @@ public class LoginFilter implements Filter {
 		log.debug("Login request to OAuth server : {}", oAuthServer.toString());
 		OAuthProperties properties = oAuthPropertyHandler.getProperties(oAuthServer);
 
-		response.sendRedirect(properties.getAccessCodeRequestUrl());
+		String state = UUID.randomUUID().toString();
+		request.getSession().setAttribute(STATE, state);
+
+		UriComponents uri = UriComponentsBuilder.fromHttpUrl(properties.getAccessCodeRequestUrl())
+			.queryParam(CLIENT_ID, properties.getClientId())
+			.queryParam(STATE, state)
+			.queryParam(REDIRECT_URI, properties.getRedirectUri())
+			.build();
+
+		response.sendRedirect(uri.toString());
 	}
 
 	private String parseProvider(HttpServletRequest httpRequest) {
