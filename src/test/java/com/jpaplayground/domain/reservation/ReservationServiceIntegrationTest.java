733460b0f76d6f622128c7e5315a36927b9d852e
honeySleepr <gruzzimo@naver.com>
2022-11-14 23:48:52 +0900

    ♻️ Reservation 조회, 수정 테스트 추가

--- src/test/java/com/jpaplayground/domain/reservation/ReservationServiceIntegrationTest.java
+++ src/test/java/com/jpaplayground/domain/reservation/ReservationServiceIntegrationTest.java
@@ -6,6 +6,7 @@ import com.jpaplayground.domain.product.ProductRepository;
 import com.jpaplayground.domain.product.exception.ProductException;
 import com.jpaplayground.domain.reservation.dto.ReservationCreateRequest;
 import com.jpaplayground.domain.reservation.dto.ReservationResponse;
+import com.jpaplayground.domain.reservation.dto.ReservationUpdateRequest;
 import com.jpaplayground.domain.reservation.exception.ReservationException;
 import com.jpaplayground.global.exception.ErrorCode;
 import com.jpaplayground.global.member.Member;
@@ -78,7 +79,6 @@ class ReservationServiceIntegrationTest {
 			LocalDateTime now = LocalDateTime.now();
 			ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
 
-			// when
 			// then
 			assertThatThrownBy(() -> reservationService.create(request, reservedProduct.getId(), seller.getId()))
 				.isInstanceOf(ReservationException.class)
@@ -91,8 +91,6 @@ class ReservationServiceIntegrationTest {
 			// given
 			LocalDateTime now = LocalDateTime.now();
 			ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
-
-			// when
 
 			// then
 			assertThatThrownBy(() -> reservationService.create(request, notReservedProduct.getId(), buyer.getId()))
@@ -108,12 +106,82 @@ class ReservationServiceIntegrationTest {
 		@Test
 		@DisplayName("판매자 또는 구매자가 제품의 예약 정보 조회 요청 시 예약 정보를 반환한다")
 		void find() {
-			// given
-
 			// when
-
+			ReservationResponse response1 = reservationService.findByProductId(reservedProduct.getId(),
+				seller.getId());
+			ReservationResponse response2 = reservationService.findByProductId(reservedProduct.getId(),
+				buyer.getId());
 			// then
+			assertThat(response1.getBuyerId()).isEqualTo(buyer.getId());
+			assertThat(response1.getSellerId()).isEqualTo(seller.getId());
+			assertThat(response1.getProductId()).isEqualTo(reservedProduct.getId());
+			assertThat(response2.getBuyerId()).isEqualTo(buyer.getId());
+			assertThat(response2.getSellerId()).isEqualTo(seller.getId());
+			assertThat(response2.getProductId()).isEqualTo(reservedProduct.getId());
+		}
 
+		@Test
+		@DisplayName("판매자나 구매자가 아닌 member가 조회 요청 시 예외가 발생한다")
+		void find_not_buyer_nor_sell() {
+			// then
+			assertThatThrownBy(() -> reservationService.findByProductId(reservedProduct.getId(), thirdPerson.getId()))
+				.isInstanceOf(ReservationException.class)
+				.hasMessage(ErrorCode.NOT_SELLER_NOR_BUYER.getMessage());
+		}
+
+		@Test
+		@DisplayName("존재하지 않는 예약을 조회하면 예외가 발생한다")
+		void find_null() {
+			// then
+			assertThatThrownBy(() -> reservationService.findByProductId(notReservedProduct.getId(), seller.getId()))
+				.isInstanceOf(ReservationException.class)
+				.hasMessage(ErrorCode.RESERVATION_NOT_FOUND.getMessage());
 		}
 	}
+
+	@Nested
+	@DisplayName("Reservation 수정 테스트")
+	class UpdateTest {
+
+		@Test
+		@DisplayName("판매자가 예약 수정을 요청하면 예약이 수정된다")
+		void update() {
+			// given
+			LocalDateTime newTime = LocalDateTime.of(2022, 12, 1, 12, 30);
+			ReservationUpdateRequest request = new ReservationUpdateRequest(newTime);
+
+			// when
+			ReservationResponse response = reservationService.update(reservedProduct.getId(), request, seller.getId());
+
+			// then
+			assertThat(response.getTimeToMeet()).isEqualTo(newTime);
+		}
+
+		@Test
+		@DisplayName("판매자가 아닌 member가 예약 수정을 요청하면 예외가 발생한다")
+		void update_not_seller() {
+			// given
+			LocalDateTime newTime = LocalDateTime.of(2022, 12, 1, 12, 30);
+			ReservationUpdateRequest request = new ReservationUpdateRequest(newTime);
+
+			// then
+			assertThatThrownBy(() -> reservationService.update(reservedProduct.getId(), request, buyer.getId()))
+				.isInstanceOf(ProductException.class)
+				.hasMessage(ErrorCode.NOT_SELLER.getMessage());
+		}
+
+		@Test
+		@DisplayName("존재하지 않는 예약의 수정을 요청하면 예외가 발생한다.")
+		void update_no_reservation() {
+			// given
+			LocalDateTime newTime = LocalDateTime.of(2022, 12, 1, 12, 30);
+			ReservationUpdateRequest request = new ReservationUpdateRequest(newTime);
+
+			// then
+			assertThatThrownBy(() -> reservationService.update(notReservedProduct.getId(), request, seller.getId()))
+				.isInstanceOf(ReservationException.class)
+				.hasMessage(ErrorCode.RESERVATION_NOT_FOUND.getMessage());
+		}
+	}
+
 }
