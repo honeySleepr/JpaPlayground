f531dbf7237af1ef9412785e1a91c9d1f9e879b6
honeySleepr <gruzzimo@naver.com>
2022-11-14 19:02:01 +0900

    ♻️ Test Data 수정
    - 테스트에 필요한 member들을 판매자, 구매자, 제 3자로 구분
    - 테스트에 필요한 product들 따로 선언(product, reservedProduct)

--- src/test/java/com/jpaplayground/domain/product/ProductServiceIntegrationTest.java
+++ src/test/java/com/jpaplayground/domain/product/ProductServiceIntegrationTest.java
@@ -31,6 +31,7 @@ class ProductServiceIntegrationTest {
 	@Autowired
 	TestData testData;
 	List<Product> allProducts;
+	Product product;
 	Member seller;
 	Member buyer;
 
@@ -38,8 +39,9 @@ class ProductServiceIntegrationTest {
 	void init() {
 		testData.init();
 		allProducts = testData.getAllProducts();
-		seller = testData.getAllMembers().get(0);
-		buyer = testData.getAllMembers().get(1);
+		product = allProducts.get(4);
+		seller = testData.getSeller();
+		buyer = testData.getBuyer();
 	}
 
 	@Test
@@ -60,7 +62,6 @@ class ProductServiceIntegrationTest {
 	@DisplayName("판매자가 자신의 product를 삭제 요청을 하면 product가 삭제된다")
 	void delete() {
 		// given
-		Product product = allProducts.get(4);
 		Long sellerId = seller.getId();
 
 		// when
@@ -74,7 +75,6 @@ class ProductServiceIntegrationTest {
 	@DisplayName("판매자가 아닌 member가 product 삭제 요청을 하면 예외가 발생한다")
 	void delete_not_seller() {
 		// given
-		Product product = allProducts.get(4);
 		Long buyerId = buyer.getId();
 
 		// when
@@ -98,7 +98,7 @@ class ProductServiceIntegrationTest {
 
 	@Nested
 	@DisplayName("Product 조회 시")
-	class FindAll {
+	class FindTest {
 
 		@Test
 		@DisplayName("Paging이 적용된 제품 목록을 반환한다")
@@ -139,7 +139,6 @@ class ProductServiceIntegrationTest {
 	@DisplayName("제품의 모든 필드를 수정 요청을 하면 모든 필드가 수정된다")
 	void update_all_fields() {
 		// given
-		Product product = allProducts.get(4);
 		ProductUpdateRequest request = new ProductUpdateRequest("수정제품", 1234567);
 		Long sellerId = seller.getId();
 
@@ -155,7 +154,6 @@ class ProductServiceIntegrationTest {
 	@DisplayName("제품의 필드 하나만을 수정 요청을 하면 해당 필드만 수정된다")
 	void update_some_fields() {
 		// given
-		Product product = allProducts.get(4);
 		Integer oldPrice = product.getPrice();
 		ProductUpdateRequest request = new ProductUpdateRequest("수정제품", null);
 		Long sellerId = seller.getId();
@@ -172,7 +170,6 @@ class ProductServiceIntegrationTest {
 	@DisplayName("판매자가 아닌 member가 product 수정 요청을 하면 예외가 발생한다")
 	void update_not_seller() {
 		// given
-		Product product = allProducts.get(4);
 		ProductUpdateRequest request = new ProductUpdateRequest("수정제품", null);
 		Long buyerId = buyer.getId();
 
--- src/test/java/com/jpaplayground/domain/reservation/ReservationServiceIntegrationTest.java
+++ src/test/java/com/jpaplayground/domain/reservation/ReservationServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.jpaplayground.domain.reservation;
 
 import com.jpaplayground.TestData;
 import com.jpaplayground.domain.product.Product;
+import com.jpaplayground.domain.product.ProductRepository;
 import com.jpaplayground.domain.product.exception.ProductException;
 import com.jpaplayground.domain.reservation.dto.ReservationCreateRequest;
 import com.jpaplayground.domain.reservation.dto.ReservationResponse;
@@ -9,11 +10,11 @@ import com.jpaplayground.domain.reservation.exception.ReservationException;
 import com.jpaplayground.global.exception.ErrorCode;
 import com.jpaplayground.global.member.Member;
 import java.time.LocalDateTime;
-import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,68 +27,93 @@ class ReservationServiceIntegrationTest {
 	@Autowired
 	ReservationService reservationService;
 	@Autowired
+	ProductRepository productRepository;
+	@Autowired
 	TestData testData;
-	List<Product> allProducts;
-	List<Member> allMembers;
+	Product notReservedProduct;
+	Product reservedProduct;
+	Member seller;
+	Member buyer;
+	Member thirdPerson;
 
 	@BeforeEach
 	void init() {
 		testData.init();
-		allProducts = testData.getAllProducts();
-		allMembers = testData.getAllMembers();
+		notReservedProduct = testData.getAllProducts().get(4);
+		reservedProduct = testData.getReservedProduct();
+		seller = testData.getSeller();
+		buyer = testData.getBuyer();
+		thirdPerson = testData.getThirdPerson();
 	}
 
-	@Test
-	@DisplayName("판매자가 자신이 등록한 제품의 예약을 요청을 하면 예약이 생성된다")
-	void create() {
-		// given
-		Member seller = allMembers.get(0);
-		Member buyer = allMembers.get(1);
-		Product product5 = allProducts.get(4);
-		LocalDateTime now = LocalDateTime.now();
-		ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
+	@Nested
+	@DisplayName("Reservation 등록 테스트")
+	class CreateTest {
 
-		// when
-		ReservationResponse response = reservationService.create(request, product5.getId(), seller.getId());
+		@Test
+		@DisplayName("판매자가 자신이 등록한 제품의 예약을 요청을 하면 예약이 생성된다")
+		void create() {
+			// given
+			LocalDateTime now = LocalDateTime.now();
+			ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
 
-		// then
-		assertThat(response.getTimeToMeet()).isEqualTo(now);
-		assertThat(response.getBuyerId()).isEqualTo(buyer.getId());
+			// when
+			ReservationResponse response = reservationService.create(request, notReservedProduct.getId(),
+				seller.getId());
+
+			// then
+			assertThat(response.getTimeToMeet()).isEqualTo(now);
+			assertThat(response.getBuyerId()).isEqualTo(buyer.getId());
+
+			Product foundProduct = productRepository.findByIdAndDeletedFalse(notReservedProduct.getId()).get();
+			Reservation reservation = foundProduct.getReservation();
+			assertThat(reservation.getTimeToMeet()).isEqualTo(now);
+			assertThat(reservation.getBuyer().getId()).isEqualTo(buyer.getId());
+		}
+
+		@Test
+		@DisplayName("이미 예약된 제품에 예약 요청을 하면 예외가 발생한다")
+		void create_already_reserved() {
+			// given
+			LocalDateTime now = LocalDateTime.now();
+			ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
+
+			// when
+			// then
+			assertThatThrownBy(() -> reservationService.create(request, reservedProduct.getId(), seller.getId()))
+				.isInstanceOf(ReservationException.class)
+				.hasMessage(ErrorCode.RESERVED.getMessage());
+		}
+
+		@Test
+		@DisplayName("판매자가 아닌 member가 제품에 예약을 요청을 하면 예외가 발생한다")
+		void create_not_seller() {
+			// given
+			LocalDateTime now = LocalDateTime.now();
+			ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
+
+			// when
+
+			// then
+			assertThatThrownBy(() -> reservationService.create(request, notReservedProduct.getId(), buyer.getId()))
+				.isInstanceOf(ProductException.class)
+				.hasMessage(ErrorCode.NOT_SELLER.getMessage());
+		}
 	}
 
-	@Test
-	@DisplayName("이미 예약된 제품에 예약 요청을 하면 예외가 발생한다")
-	void create_already_reserved() {
-		// given
-		Member seller = allMembers.get(0);
-		Member buyer = allMembers.get(1);
-		Product reservedProduct = allProducts.get(1);
-		LocalDateTime now = LocalDateTime.now();
-		ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
+	@Nested
+	@DisplayName("Reservation 조회 테스트")
+	class FindTest {
 
-		// when
+		@Test
+		@DisplayName("판매자 또는 구매자가 제품의 예약 정보 조회 요청 시 예약 정보를 반환한다")
+		void find() {
+			// given
 
-		// then
-		assertThatThrownBy(() -> reservationService.create(request, reservedProduct.getId(), seller.getId()))
-			.isInstanceOf(ReservationException.class)
-			.hasMessage(ErrorCode.RESERVED.getMessage());
+			// when
+
+			// then
+
+		}
 	}
-
-	@Test
-	@DisplayName("판매자가 아닌 member가 제품에 예약을 요청을 하면 예외가 발생한다")
-	void create_not_seller() {
-		// given
-		Member buyer = allMembers.get(1);
-		Product product5 = allProducts.get(4);
-		LocalDateTime now = LocalDateTime.now();
-		ReservationCreateRequest request = new ReservationCreateRequest(buyer.getId(), now);
-
-		// when
-
-		// then
-		assertThatThrownBy(() -> reservationService.create(request, product5.getId(), buyer.getId()))
-			.isInstanceOf(ProductException.class)
-			.hasMessage(ErrorCode.NOT_SELLER.getMessage());
-	}
-
 }
