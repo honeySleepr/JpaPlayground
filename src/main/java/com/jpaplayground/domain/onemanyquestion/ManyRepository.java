54a9d5eb4dba46ac09ed0f34c3222f7cb4202234
honeySleepr <gruzzimo@naver.com>
2022-12-03 16:27:08 +0900

    Revert "💩 one many 관계 질문 용 코드(revert 예정)"
    This reverts commit 28a77b979657d982cf3126e1ee0bbddf906c473e.

--- src/main/java/com/jpaplayground/domain/onemanyquestion/ManyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jpaplayground.domain.onemanyquestion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManyRepository extends JpaRepository<Many, Long> {
-}
--- src/main/java/com/jpaplayground/domain/onemanyquestion/OneRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jpaplayground.domain.onemanyquestion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OneRepository extends JpaRepository<One, Long> {
-}
